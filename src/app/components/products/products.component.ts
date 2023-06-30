eb6a91945ea90f0639d5e542fbb87086262d2b95
SantiagoOspinaO <santiago.m200@outlook.es>
2023-06-30 15:43:53 -0500

    Conociendo los services

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
     selector: 'app-products',
@@ -49,13 +50,15 @@ export class ProductsComponent implements OnInit {
         },
     ];
 
-    constructor() { }
+    constructor(private storeService: StoreService) {
+        this.myShoppingCart = this.storeService.getShoppingCart();
+    }
 
     ngOnInit(): void {
     }
 
     onAddToShoppingCart(product: Product) {
-        this.myShoppingCart.push(product)
-        this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0)
+        this.storeService.addProduct(product);
+        this.total = this.storeService.getTotal();
     }
 }
