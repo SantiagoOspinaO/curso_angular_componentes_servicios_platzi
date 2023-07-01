d0ec1fe1f7de77f3d5399acf527c55d9c4e721b4
SantiagoOspinaO <santiago.m200@outlook.es>
2023-06-30 23:08:35 -0500

    Conociendo las directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { ReplaceVowelPipe } from './pipes/replace-vowel.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
     declarations: [
@@ -22,7 +23,8 @@ import { ReplaceVowelPipe } from './pipes/replace-vowel.pipe';
         NavComponent,
         ReversePipe,
         TimeAgoPipe,
-        ReplaceVowelPipe
+        ReplaceVowelPipe,
+        HighlightDirective
     ],
     imports: [
         BrowserModule,
--- src/app/pipes/replace-vowel.pipe.ts
+++ src/app/pipes/replace-vowel.pipe.ts
@@ -11,7 +11,7 @@ export class ReplaceVowelPipe implements PipeTransform {
         newString = newString.replace(/e/gi, '3');
         newString = newString.replace(/i/gi, '1');
         newString = newString.replace(/o/gi, '0');
-        newString = newString.replace(/u/gi, '|_|');
+        newString = newString.replace(/u/gi, 'u');
         return newString;
     }
 }
