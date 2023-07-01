c9d43921ae1871c1bb5b27e77217c9a12e6c0b6c
SantiagoOspinaO <santiago.m200@outlook.es>
2023-06-30 22:22:31 -0500

    COnstruyendo tu propio pipe

---
+++ src/app/pipes/replace-vowel.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ReplaceVowelPipe } from './replace-vowel.pipe';
+
+describe('ReplaceVowelPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ReplaceVowelPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/replace-vowel.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'replaceVowel'
+})
+export class ReplaceVowelPipe implements PipeTransform {
+
+    transform(value: string,): string {
+
+        let newString = value.replace(/a/gi, '4');
+        newString = newString.replace(/e/gi, '3');
+        newString = newString.replace(/i/gi, '1');
+        newString = newString.replace(/o/gi, '0');
+        newString = newString.replace(/u/gi, '|_|');
+        return newString;
+    }
+}
